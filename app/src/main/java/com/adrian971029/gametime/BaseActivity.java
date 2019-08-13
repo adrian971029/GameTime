6efa04f84eae0d6dbb6b1a77531b3f76ec6cb90d
Adrian Del Toro <adrian.pena@solinftec.com.br>
2019-08-13 09:37:53 -0300

    Configuracoes de dependencias, fontes e cores.

---
+++ app/src/main/java/com/adrian971029/gametime/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.adrian971029.gametime;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+}
