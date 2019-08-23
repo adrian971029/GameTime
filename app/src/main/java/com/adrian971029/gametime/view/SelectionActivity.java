aa3b441d7cc8d5b4f621e2ef2df1c8bad76fc7d9
Adrian Del Toro <adrian.pena@solinftec.com.br>
2019-08-23 10:49:29 -0300

    Implementado navegation drawer na tela de selecao.

--- app/src/main/java/com/adrian971029/gametime/view/SelectionActivity.java
+++ app/src/main/java/com/adrian971029/gametime/view/SelectionActivity.java
@@ -4,7 +4,13 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
@@ -12,16 +18,23 @@ import com.adrian971029.gametime.base.BaseFragment;
 import com.adrian971029.gametime.R;
 import com.adrian971029.gametime.adapter.ScreenSlidePagerAdapter;
 import com.adrian971029.gametime.helper.ZoomOutPageTransformer;
+import com.google.android.material.navigation.NavigationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SelectionActivity extends BaseFragment {
+public class SelectionActivity extends BaseFragment implements NavigationView.OnNavigationItemSelectedListener{
 
     public static final int NUM_PAGES = 3;
 
     @BindView(R.id.pager)
     ViewPager mPager;
+    @BindView(R.id.toolBar)
+    Toolbar toolbar;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
+    @BindView(R.id.navView)
+    NavigationView navigationView;
 
     private Context context;
     private Resources resources;
@@ -36,6 +49,13 @@ public class SelectionActivity extends BaseFragment {
         ButterKnife.bind(this);
         context = getApplicationContext();
         resources = context.getResources();
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(SelectionActivity.this);
 
         mediaPlayerChangePage = MediaPlayer.create(getApplicationContext(), R.raw.swipe_pager_sound);
         setupPager();
@@ -66,7 +86,15 @@ public class SelectionActivity extends BaseFragment {
 
     @Override
     public void onBackPressed() {
-        createDialogSair(this, resources.getString(R.string.lbl_deseja_sair));
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            createDialogSair(this, resources.getString(R.string.lbl_deseja_sair));
+        }
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        return false;
+    }
 }
