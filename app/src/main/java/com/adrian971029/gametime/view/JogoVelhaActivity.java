8c0dac0d539d0bb4198ff35ec2bd7f200fabca52
Otávio Freitas <otavio.freitasb@gmail.com>
2019-08-28 14:30:12 -0300

    criando layout jogo da velha

--- app/src/main/java/com/adrian971029/gametime/view/JogoVelhaActivity.java
+++ app/src/main/java/com/adrian971029/gametime/view/JogoVelhaActivity.java
@@ -1,7 +1,6 @@
 package com.adrian971029.gametime.view;
 
 import android.os.Bundle;
-
 import com.adrian971029.gametime.R;
 import com.adrian971029.gametime.base.BaseActivity;
 
@@ -11,5 +10,7 @@ public class JogoVelhaActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo_velha);
+
     }
+
 }
